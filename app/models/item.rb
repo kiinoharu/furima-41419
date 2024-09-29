022dce1754f015fd7c40b410980e76dab6640981
Pretty-dogg <haruki89618@gmail.com>
2024-09-29 17:32:48 +0900

    暫定バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :image, presence: true
+
+  validates :item_name,              presence: true
+  validates :item_description,       presence: true
+  validates :category_id,            presence: true
+  validates :item_condition_id,      presence: true
+  validates :shipping_fee_burden_id, presence: true
+  validates :prefecture_id,          presence: true
+  validates :shipping_day_id,        presence: true
+  validates :price,                  presence: true
 end
