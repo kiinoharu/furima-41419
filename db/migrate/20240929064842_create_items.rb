2840ca085806389b748abcd090ee3699ad2a749e
Pretty-dogg <haruki89618@gmail.com>
2024-09-29 16:01:40 +0900

    テーブル作成、db:migrate実行

--- db/migrate/20240929064842_create_items.rb
+++ db/migrate/20240929064842_create_items.rb
@@ -1,6 +1,15 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
+      t.string     :item_name,              null: false, default: ""
+      t.text       :item_description,       null: false
+      t.integer    :category_id,            null: false
+      t.integer    :item_condition_id,      null: false
+      t.integer    :shipping_fee_burden_id, null: false
+      t.integer    :prefecture_id,          null: false
+      t.integer    :shipping_day_id,        null: false
+      t.integer    :price,                  null: false
+      t.references :user,                   null: false, foreign_key: true
 
       t.timestamps
     end
