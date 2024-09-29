4e43dc1130306a443bdca819647fac1b82ae4031
Pretty-dogg <haruki89618@gmail.com>
2024-09-29 18:09:32 +0900

    category_idを含めたデータの保存処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.new(item.params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id)
+  end
 end
