06a91a7d71405e04301f28544a5a8abcb7d89a07
Pretty-dogg <haruki89618@gmail.com>
2024-09-29 18:20:49 +0900

    createアクション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item.params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
