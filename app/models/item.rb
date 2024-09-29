ddae23c9b2f841b870d45d7717fbc37463d0bd69
Pretty-dogg <haruki89618@gmail.com>
2024-09-29 19:02:35 +0900

    iteｍモデル編集
    ActiveHash関連のバリデーション編集＋アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,12 +7,16 @@ class Item < ApplicationRecord
   validates :item_name,              presence: true
   validates :item_description,       presence: true
   validates :category_id,            numericality: { other_than: 1, message: "can't be blank" }
-  validates :item_condition_id,      presence: true
-  validates :shipping_fee_burden_id, presence: true
-  validates :prefecture_id,          presence: true
-  validates :shipping_day_id,        presence: true
+  validates :item_condition_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_burden_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,          numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id,        numericality: { other_than: 1, message: "can't be blank" }
   validates :price,                  presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :item_condition
+  belongs_to :shipping_fee_burden
+  belongs_to :prefecture
+  belongs_to :shipping_day
 end
