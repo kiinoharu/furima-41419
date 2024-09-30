9e10308b743b8dc3234f8863f7d003c6568d6194
Pretty-dogg <haruki89618@gmail.com>
2024-09-30 22:52:32 +0900

    itemモデル修正
    priceカラムのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,8 @@ class Item < ApplicationRecord
   validates :prefecture_id,          numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id,        numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be between ¥300 and ¥9,999,999' }
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'must be between ¥300 and ¥9,999,999' }
+  validates :price, numericality: { only_integer: true, message: 'is not a number' }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
