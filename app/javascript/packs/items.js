2cb188465c3cc83b733c782ea6f380810925df44
Pretty-dogg <haruki89618@gmail.com>
2024-10-01 22:56:07 +0900

    items.js修正
    記述内容変更＋ renderメソッドに対応したイベントを追加

--- app/javascript/packs/items.js
+++ app/javascript/packs/items.js
@@ -1,24 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
+const price = () => {
   // 価格入力フィールドを取得
   const priceInput = document.getElementById("item-price");
   
   // 販売手数料と利益を表示するエリアを取得
   const addTaxPrice = document.getElementById("add-tax-price");
   const profit = document.getElementById("profit");
-
   // 価格が入力された際の処理
   priceInput.addEventListener('input', () => {
-    const price = parseInt(priceInput.value); // 入力された価格を整数に変換
+    const price = priceInput.value; 
+    
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(price * 0.1);
+    
+    const profit = document.getElementById("profit");
+    profit.innerHTML = Math.floor(price - (price * 0.1))
+  })
+};
 
-    if (!isNaN(price) && price >= 300 && price <= 9999999) { // 価格が範囲内かどうかをチェック
-      const fee = Math.floor(price * 0.1); // 販売手数料（10%）を計算し、小数点以下を切り捨て
-      const netProfit = Math.floor(price - fee); // 販売利益を計算し、小数点以下を切り捨て
-
-      addTaxPrice.textContent = fee; // 手数料を表示
-      profit.textContent = netProfit; // 利益を表示
-    } else {
-      addTaxPrice.textContent = ''; // 範囲外の価格の場合、表示をクリア
-      profit.textContent = '';
-    }
-  });
-});
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
