4b8e347105071dcc40001c6d9e80e1d5da5c8da7
Pretty-dogg <haruki89618@gmail.com>
2024-10-09 22:48:09 +0900

    orders_controller.rb/各情報を保存できるよ記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,22 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-    @order = Order.all
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+      render 'index', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :phone_number).merge(token: params[:token])
   end
 end
