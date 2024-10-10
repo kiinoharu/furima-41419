81bcaf89bb29a73f5d756e9c3764761c4614d3a1
Pretty-dogg <haruki89618@gmail.com>
2024-10-10 22:32:47 +0900

    クレカ決済/token関連設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     if @order.valid?
+      pay_item
       @order.save
       redirect_to root_path
     else
@@ -18,5 +19,14 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :phone_number).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = 'sk_test_a5e12b77e1f67c83ec145eaf' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,8 @@
 class Order < ApplicationRecord
   attr_accessor :token
 
+  validates :token, presence: true
+
   belongs_to :user
   belongs_to :item
   has_one :shipping_detail
