129ddd4663e906cf1e0d6a0b3ef0b30ce0683cc5
Pretty-dogg <haruki89618@gmail.com>
2024-10-11 23:01:01 +0900

    orders_controller.rb編集/フォームオブジェクトのクラスへ対応

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
-    @order = Order.new
+    @order_shipping = OrderShipping.new # 正しいクラス名を使用
   end
 
   def create
-    @order = Order.new(order_params)
-    if @order.valid?
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
       pay_item
-      @order.save
+      @order_shipping.save
       redirect_to root_path
     else
       render 'index', status: :unprocessable_entity
@@ -18,15 +19,21 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:postal_code, :prefecture_id, :city, :address, :phone_number).merge(token: params[:token])
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :address, :phone_number).merge(
+      token: params[:token], item_id: params[:item_id], user_id: current_user.id
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item
-    Payjp.api_key = 'sk_test_a5e12b77e1f67c83ec145eaf' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp.api_key = 'sk_test_a5e12b77e1f67c83ec145eaf' # PAY.JPの秘密鍵
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
       card: order_params[:token], # カードトークン
-      currency: 'jpy' # 通貨の種類（日本円）
+      currency: 'jpy' # 日本円
     )
   end
 end
