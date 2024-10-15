c333997075d38fabfb7955230542cdac14439abc
Pretty-dogg <haruki89618@gmail.com>
2024-10-15 21:49:57 +0900

    order_shipping.rbに重複しているバリデーション削除

--- app/models/shipping_detail.rb
+++ app/models/shipping_detail.rb
@@ -1,13 +1,6 @@
 class ShippingDetail < ApplicationRecord
   belongs_to :order
 
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  validates :prefecture_id, presence: true, numericality: { other_than: 0, message: 'must be selected' }
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true,
-                           format: { with: /\A\d{10,11}\z/, message: 'is invalid. Enter a 10 or 11 digit number.' }
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 end
