2a5454e7369d72a5c99acaac7302139177e82afa
Pretty-dogg <haruki89618@gmail.com>
2024-10-15 17:24:00 +0900

    iteｍモデルにsold_outカラム追加修正

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_08_123506) do
     t.integer "shipping_day_id", null: false
     t.integer "price", null: false
     t.bigint "user_id", null: false
+    t.boolean "sold_out", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
