a0cea209799ffdb2f6c14be3dea48871c91519c7
Pretty-dogg <haruki89618@gmail.com>
2024-10-15 16:11:32 +0900

    gon適用による修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -20,6 +20,7 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :address, :phone_number).merge(
+    params.require(:order_shipping).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(
       token: params[:token], item_id: params[:item_id], user_id: current_user.id
     )
   end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_4b81f3ec026a105be517ac2e')
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -29,3 +30,4 @@ const pay = () => {
 };
 
 window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
