e18d8aa9ff8eced03258340457eef2820a9b1b2b
Pretty-dogg <haruki89618@gmail.com>
2024-10-16 20:43:42 +0900

    spec/factories/order_shippings.rb修正
    associationメソッドを使用し、関連するiteｍやuserを紐付け。

--- spec/factories/order_shippings.rb
+++ spec/factories/order_shippings.rb
@@ -1,13 +1,14 @@
 FactoryBot.define do
   factory :order_shipping do
     postal_code      { '123-4567' }
-    prefecture_id    { 1 }  # 0以外の値
+    prefecture_id    { 1 }
     city             { 'Tokyo' }
     address          { '1-1-1 Shibuya' }
     building_name    { 'Shibuya Heights' }
     phone_number     { '09012345678' }
     token            { 'tok_abcdefghijk00000000000000000' }
-    item_id          { 1 }
-    user_id          { 1 }
+
+    association :item
+    association :user
   end
 end
