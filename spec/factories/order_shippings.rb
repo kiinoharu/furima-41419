13297c41a8dc4d263bc7439e3bff2cbd0dc1a8d7
Pretty-dogg <haruki89618@gmail.com>
2024-10-16 21:38:10 +0900

    spec/factories/order_shippings.rb修正

--- spec/factories/order_shippings.rb
+++ spec/factories/order_shippings.rb
@@ -7,8 +7,5 @@ FactoryBot.define do
     building_name    { 'Shibuya Heights' }
     phone_number     { '09012345678' }
     token            { 'tok_abcdefghijk00000000000000000' }
-
-    association :item
-    association :user
   end
 end
